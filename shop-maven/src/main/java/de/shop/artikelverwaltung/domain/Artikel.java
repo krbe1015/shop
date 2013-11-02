43f4a28b0b83137403ce48a007bba2654e60df76
Tobias Cabrera Cano <cato1011@hs-karlsruhe.de>
2013-11-02 16:44:49 +0100

    alles auf default

---
+++ shop-maven/src/main/java/de/shop/artikelverwaltung/domain/Artikel.java
@@ -0,0 +1,94 @@
+package de.shop.artikelverwaltung.domain;
+
+public class Artikel {
+	
+	long id;
+	String bezeichnung;
+	double preis;
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+	/**
+	 * @return the bezeichnung
+	 */
+	public String getBezeichnung() {
+		return bezeichnung;
+	}
+	/**
+	 * @param bezeichnung the bezeichnung to set
+	 */
+	public void setBezeichnung(String bezeichnung) {
+		this.bezeichnung = bezeichnung;
+	}
+	/**
+	 * @return the preis
+	 */
+	public double getPreis() {
+		return preis;
+	}
+	/**
+	 * @param preis the preis to set
+	 */
+	public void setPreis(double preis) {
+		this.preis = preis;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((bezeichnung == null) ? 0 : bezeichnung.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		long temp;
+		temp = Double.doubleToLongBits(preis);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Artikel other = (Artikel) obj;
+		if (bezeichnung == null) {
+			if (other.bezeichnung != null)
+				return false;
+		} else if (!bezeichnung.equals(other.bezeichnung))
+			return false;
+		if (id != other.id)
+			return false;
+		if (Double.doubleToLongBits(preis) != Double
+				.doubleToLongBits(other.preis))
+			return false;
+		return true;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Artikel [id=" + id + ", bezeichnung=" + bezeichnung
+				+ ", preis=" + preis + "]";
+	}
+	
+	
+
+}
