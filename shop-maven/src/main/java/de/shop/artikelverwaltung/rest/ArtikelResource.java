4b21a5bdb359c71a37f45c06bc0a6d70c3104abe
dise1012 <dise1012@users.noreply.github.com>
2013-11-06 19:31:41 +0100

    Update ArtikelResource.java

--- shop-maven/src/main/java/de/shop/artikelverwaltung/rest/ArtikelResource.java
+++ shop-maven/src/main/java/de/shop/artikelverwaltung/rest/ArtikelResource.java
@@ -1,5 +1,97 @@
 package de.shop.artikelverwaltung.rest;
 
-public class ArtikelResource {
+import static de.shop.util.Constants.SELF_LINK;
+//import static de.shop.util.Constants.ADD_LINK;
+//import static de.shop.util.Constants.REMOVE_LINK;
+//import static de.shop.util.Constants.UPDATE_LINK;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.MediaType.APPLICATION_XML;
+import static javax.ws.rs.core.MediaType.TEXT_XML;
 
+import java.net.URI;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Link;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import de.shop.artikelverwaltung.domain.Artikel;
+import de.shop.util.MockService;
+import de.shop.util.rest.UriHelper;
+import de.shop.util.rest.NotFoundException;
+
+
+@Path("/artikel")
+@Produces({ APPLICATION_JSON, APPLICATION_XML + ";qs=0.75", TEXT_XML + ";qs=0.5" })
+@Consumes
+public class ArtikelResource {
+	
+	@Context
+	private UriInfo uriInfo;
+	
+	@Inject
+	private UriHelper uriHelper;
+	
+	// @Inject
+	// private ArtikelResource artikelResource;
+	
+	@GET
+	@Path("{id:[1-9][0-9]*}")
+	public Response findArtikelById(@PathParam("id") Long id, @Context UriInfo uriInfo) {
+		final Artikel artikel = MockService.findArtikelById(id);
+		if (artikel == null) {
+			throw new NotFoundException("Kein Artikel mit der ID " + id + " gefunden.");
+		}
+
+		return Response.ok(artikel)
+	                   .links(getTransitionalLinks(artikel, uriInfo))
+	                   .build();
+	}
+	
+/*	
+ * public void setStructuralLinks(Bestellung bestellung, UriInfo uriInfo) {
+		// URI fuer Kunde setzen
+		final Artikel artikel = artikel.getArtikel();
+		if (artic != null) {
+			final URI kundeUri = kundeResource.getUriKunde(bestellung.getKunde(), uriInfo);
+			bestellung.setKundeUri(kundeUri);
+		}
+	}
+*/
+	
+	private Link[] getTransitionalLinks(Artikel artikel, UriInfo uriInfo) {
+		final Link self = Link.fromUri(getUriArtikel(artikel, uriInfo))
+                              .rel(SELF_LINK)
+                              .build();
+
+		return new Link[] { self };
+	}
+	
+	public URI getUriArtikel(Artikel artikel, UriInfo uriInfo) {
+		return uriHelper.getUri(ArtikelResource.class, "findArtikelById", artikel.getId(), uriInfo);
+	}
+	
+	@POST
+	@Consumes({APPLICATION_JSON, APPLICATION_XML, TEXT_XML})
+	@Produces
+	public Response createArtikel(Artikel artikel) {
+		artikel = MockService.createArtikel(artikel);
+		return Response.created(getUriArtikel(artikel, uriInfo))
+		           .build();
+	}
+	
+	@PUT
+	@Consumes({APPLICATION_JSON, APPLICATION_XML, TEXT_XML})
+	@Produces
+	public void updateArtikel(Artikel artikel) {
+		MockService.updateArtikel(artikel);
+	}
 }
