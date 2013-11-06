cc581ac02c318c9cd42c55273278d2c08937e90b
dise1012 <dise1012@users.noreply.github.com>
2013-11-06 19:32:41 +0100

    Update Bestellung.java

--- shop-maven/src/main/java/de/shop/bestellverwaltung/domain/Bestellung.java
+++ shop-maven/src/main/java/de/shop/bestellverwaltung/domain/Bestellung.java
@@ -15,7 +15,6 @@ public class Bestellung implements Serializable {
 	private Long id;
 	private boolean ausgeliefert;
 	
-	
 	@XmlTransient
 	private AbstractKunde kunde;
 	
@@ -46,12 +45,20 @@ public class Bestellung implements Serializable {
 	public void setKundeUri(URI kundeUri) {
 		this.kundeUri = kundeUri;
 	}
-	
+	@Override
+	public String toString() {
+		return "Bestellung [id=" + id + ", ausgeliefert=" + ausgeliefert
+				+ ", kunde=" + kunde + ", kundeUri=" + kundeUri + "]";
+	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + (ausgeliefert ? 1231 : 1237);
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((kunde == null) ? 0 : kunde.hashCode());
+		result = prime * result
+				+ ((kundeUri == null) ? 0 : kundeUri.hashCode());
 		return result;
 	}
 	@Override
@@ -62,18 +69,25 @@ public class Bestellung implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		final Bestellung other = (Bestellung) obj;
+		Bestellung other = (Bestellung) obj;
+		if (ausgeliefert != other.ausgeliefert)
+			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
-		}
-		else if (!id.equals(other.id))
+		} else if (!id.equals(other.id))
+			return false;
+		if (kunde == null) {
+			if (other.kunde != null)
+				return false;
+		} else if (!kunde.equals(other.kunde))
+			return false;
+		if (kundeUri == null) {
+			if (other.kundeUri != null)
+				return false;
+		} else if (!kundeUri.equals(other.kundeUri))
 			return false;
 		return true;
 	}
 	
-	@Override
-	public String toString() {
-		return "Bestellung [id=" + id + ", ausgeliefert=" + ausgeliefert + ", kundeUri=" + kundeUri + "]";
-	}
 }
