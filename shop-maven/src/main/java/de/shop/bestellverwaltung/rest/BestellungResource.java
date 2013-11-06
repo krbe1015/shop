6e5a7c72338e75fd0564c10df474679aea32e903
dise1012 <dise1012@users.noreply.github.com>
2013-11-06 19:58:37 +0100

    Update BestellungResource.java

--- shop-maven/src/main/java/de/shop/bestellverwaltung/rest/BestellungResource.java
+++ shop-maven/src/main/java/de/shop/bestellverwaltung/rest/BestellungResource.java
@@ -1,9 +1,12 @@
-package de.shop.artikelverwaltung.rest;
+package de.shop.bestellverwaltung.rest;
 
 import static de.shop.util.Constants.SELF_LINK;
-//import static de.shop.util.Constants.ADD_LINK;
-//import static de.shop.util.Constants.REMOVE_LINK;
-//import static de.shop.util.Constants.UPDATE_LINK;
+// import static de.shop.util.Constants.ADD_LINK;
+// import static de.shop.util.Constants.FIRST_LINK;
+// import static de.shop.util.Constants.LAST_LINK;
+// import static de.shop.util.Constants.REMOVE_LINK;
+// import static de.shop.util.Constants.SELF_LINK;
+// import static de.shop.util.Constants.UPDATE_LINK;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static javax.ws.rs.core.MediaType.APPLICATION_XML;
 import static javax.ws.rs.core.MediaType.TEXT_XML;
@@ -13,8 +16,8 @@ import java.net.URI;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -23,75 +26,81 @@ import javax.ws.rs.core.Link;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
-import de.shop.artikelverwaltung.domain.Artikel;
+// import de.shop.artikelverwaltung.domain.Artikel;
+import de.shop.bestellverwaltung.domain.Bestellung;
+import de.shop.kundenverwaltung.domain.AbstractKunde;
+import de.shop.kundenverwaltung.rest.KundeResource;
 import de.shop.util.MockService;
 import de.shop.util.rest.UriHelper;
 import de.shop.util.rest.NotFoundException;
 
-
-@Path("/artikel")
+@Path("/bestellungen")
 @Produces({ APPLICATION_JSON, APPLICATION_XML + ";qs=0.75", TEXT_XML + ";qs=0.5" })
 @Consumes
-public class ArtikelResource {
-	
+public class BestellungResource {
 	@Context
 	private UriInfo uriInfo;
 	
 	@Inject
 	private UriHelper uriHelper;
 	
-	// @Inject
-	// private ArtikelResource artikelResource;
+	@Inject
+	private KundeResource kundeResource;
 	
 	@GET
 	@Path("{id:[1-9][0-9]*}")
-	public Response findArtikelById(@PathParam("id") Long id, @Context UriInfo uriInfo) {
-		final Artikel artikel = MockService.findArtikelById(id);
-		if (artikel == null) {
-			throw new NotFoundException("Kein Artikel mit der ID " + id + " gefunden.");
+	public Response findBestellungById(@PathParam("id") Long id) {
+		// TODO Anwendungskern statt MockService, Verwendung von Locale
+		final Bestellung bestellung = MockService.findBestellungById(id);
+		if (bestellung == null) {
+			throw new NotFoundException("Keine Bestellung mit der ID " + id + " gefunden.");
 		}
-
-		return Response.ok(artikel)
-	                   .links(getTransitionalLinks(artikel, uriInfo))
-	                   .build();
+		
+		setStructuralLinks(bestellung, uriInfo);
+		
+		// Link-Header setzen
+		final Response response = Response.ok(bestellung)
+                                          .links(getTransitionalLinks(bestellung, uriInfo))
+                                          .build();
+		
+		return response;
 	}
 	
-/*	
- * public void setStructuralLinks(Bestellung bestellung, UriInfo uriInfo) {
+	public void setStructuralLinks(Bestellung bestellung, UriInfo uriInfo) {
 		// URI fuer Kunde setzen
-		final Artikel artikel = artikel.getArtikel();
-		if (artic != null) {
+		final AbstractKunde kunde = bestellung.getKunde();
+		if (kunde != null) {
 			final URI kundeUri = kundeResource.getUriKunde(bestellung.getKunde(), uriInfo);
 			bestellung.setKundeUri(kundeUri);
 		}
 	}
-*/
 	
-	private Link[] getTransitionalLinks(Artikel artikel, UriInfo uriInfo) {
-		final Link self = Link.fromUri(getUriArtikel(artikel, uriInfo))
+	private Link[] getTransitionalLinks(Bestellung bestellung, UriInfo uriInfo) {
+		final Link self = Link.fromUri(getUriBestellung(bestellung, uriInfo))
                               .rel(SELF_LINK)
                               .build();
-
 		return new Link[] {self};
 	}
 	
-	public URI getUriArtikel(Artikel artikel, UriInfo uriInfo) {
-		return uriHelper.getUri(ArtikelResource.class, "findArtikelById", artikel.getId(), uriInfo);
+	public URI getUriBestellung(Bestellung bestellung, UriInfo uriInfo) {
+		return uriHelper.getUri(BestellungResource.class, "findBestellungById", bestellung.getId(), uriInfo);
 	}
 	
 	@POST
 	@Consumes({APPLICATION_JSON, APPLICATION_XML, TEXT_XML })
 	@Produces
-	public Response createArtikel(Artikel artikel) {
-		artikel = MockService.createArtikel(artikel);
-		return Response.created(getUriArtikel(artikel, uriInfo))
+	public Response createBestellung(Bestellung bestellung) {
+		bestellung = MockService.createBestellung(bestellung);
+		setStructuralLinks(bestellung, uriInfo);
+		return Response.created(getUriBestellung(bestellung, uriInfo))
 		           .build();
 	}
 	
 	@PUT
 	@Consumes({APPLICATION_JSON, APPLICATION_XML, TEXT_XML })
 	@Produces
-	public void updateArtikel(Artikel artikel) {
-		MockService.updateArtikel(artikel);
+	public void updateBestellung(Bestellung bestellung) {
+		MockService.updateBestellung(bestellung);
 	}
+	
 }
